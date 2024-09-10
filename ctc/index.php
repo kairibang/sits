2d0c951a7cb8e7cf3768ca58a2913cf296522b27
Kairi <o.b.pigon@gmail.com>
2024-09-10 16:43:06 +0800

    Add files via upload
    my version 1

---
+++ ctc/index.php
@@ -0,0 +1,26 @@
+
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="my-navbar">
+        <div class="my-nav-brand">
+            <p>Just For Fun</p>
+        </div>
+        <div class="my-nav-category">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                    <a href="about.php">About</a>
+                    <a href="">Contacts</a>
+                    <a href="">Sign in</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <img src="img/index-front.png" style="width: 100%">
+</body>
+</html>
